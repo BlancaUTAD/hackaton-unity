7ff62836465be3240209d722fcb3b82241ad0d17
Alberto <alberto.juan@live.u-tad.com>
2018-05-29 00:10:10 +0200

    Se lanzan las 5 escenas de forma aleatoria sin repetirse

--- Hackaton/Assets/Scripts/ControlFlowScenes.cs
+++ Hackaton/Assets/Scripts/ControlFlowScenes.cs
@@ -5,25 +5,69 @@ using UnityEngine;
 
 public class ControlFlowScenes : MonoBehaviour {
 
-	private bool existLevel;
+	private int currentLevel;
+    private List<int> previousLevels;
+    private int random;
 
 	void Start () 
 	{
-		existLevel = false;
-		loadSceneRandom ();
-	}
+        currentLevel = -1;
+        previousLevels = new List<int>();
+        loadSceneRandom ();
 
-	public void loadSceneRandom ()
-	{
-		int random = Random.Range(1,1);
+    }
 
-		if (existLevel) 
-			SceneManager.UnloadSceneAsync (5);
+    public void loadSceneRandom()
+    {
+        if (previousLevels.Count < 5)
+        {
+            if (currentLevel > 0)
+            {
+                bool existLevel = true;
+                while (existLevel)
+                {
+                    random = Random.Range(1, 6);
+                    existLevel = false;
+                    Debug.Log("previousLevels: " + previousLevels);
+                    for (int i = 0; i < previousLevels.Count; i++)
+                    {
+                        if (previousLevels[i] == random)
+                        {
+                            existLevel = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                random = Random.Range(1, 6);
+            }
 
-		if (random == 1) 
-		{
-			SceneManager.LoadScene ("Scene5", LoadSceneMode.Additive);
-			existLevel = true;
-		}
-	}
+            if (currentLevel > 0)
+                SceneManager.UnloadSceneAsync(currentLevel);
+
+            if (random == 1)
+            {
+                SceneManager.LoadScene("Scene1", LoadSceneMode.Additive);
+            }
+            else if (random == 2)
+            {
+                SceneManager.LoadScene("Scene2", LoadSceneMode.Additive);
+            }
+            else if (random == 3)
+            {
+                SceneManager.LoadScene("Scene3", LoadSceneMode.Additive);
+            }
+            else if (random == 4)
+            {
+                SceneManager.LoadScene("Scene4", LoadSceneMode.Additive);
+            }
+            else if (random == 5)
+            {
+                SceneManager.LoadScene("Scene5", LoadSceneMode.Additive);
+            }
+            currentLevel = random;
+            previousLevels.Add(currentLevel);
+        }
+    }
 }
